9d665ed54a2a5814495150424c3133a6eaf83c54
fiking <1129277432@qq.com>
2024-11-25 17:32:49 +0800

    [swiftui] add hello_javascriptcore project

---
+++ application_frameworks/swiftui/hello_javascriptcore/hello_javascriptcore/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  hello_javascriptcore
+//
+//  Created by language-vm on 2024/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ application_frameworks/swiftui/hello_javascriptcore/hello_javascriptcore/hello_javascriptcoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  hello_javascriptcoreApp.swift
+//  hello_javascriptcore
+//
+//  Created by language-vm on 2024/11/25.
+//
+
+import SwiftUI
+
+@main
+struct hello_javascriptcoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
