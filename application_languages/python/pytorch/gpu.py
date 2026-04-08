98bb21c87652ba69fc5a1762c47d7f94b397b334
fiking <1129277432@qq.com>
2026-04-08 18:08:31 +0800

    [python-pytorch] add deep learning cases

---
+++ application_languages/python/pytorch/gpu.py
@@ -0,0 +1,18 @@
+import torch
+
+from torch import nn
+
+print(torch.device('cpu'))
+print(torch.device('cuda'))
+print(torch.device('cuda:1'))
+
+print(torch.cuda.device_count())
+
+
+def try_gpu(i = 0):
+  if torch.cuda.device_count() >= i + 1:
+    return torch.device(f'cuda:{i}')
+  return torch.device('cpu')
+
+def try_all_gpus():
+  devices = [torch.device(f'cuda:{i}') ]
